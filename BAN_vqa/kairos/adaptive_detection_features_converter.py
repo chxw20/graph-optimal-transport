c4e1be1de1e793487fccdfd631d994ccfd7095d4
chxw20 <xiwenche@andrew.cmu.edu>
2021-02-24 19:00:39 +0800

    updates

--- BAN_vqa/kairos/adaptive_detection_features_converter.py
+++ BAN_vqa/kairos/adaptive_detection_features_converter.py
@@ -70,9 +70,10 @@ def extract(split, infiles, task='scenario_data'):
         num_boxes = 0
         for infile in infiles:
             print("reading tsv...%s" % infile)
-            with open(infile, "r+") as tsv_in_file:
+            with open(infile, "r") as tsv_in_file:
                 reader = csv.DictReader(tsv_in_file, delimiter='\t', fieldnames=FIELDNAMES)
                 for item in reader:
+                    print(item['num_boxes'])
                     item['num_boxes'] = int(item['num_boxes'])
                     image_id = int(item['image_id'])
                     if image_id in imgids:
@@ -101,7 +102,6 @@ def extract(split, infiles, task='scenario_data'):
         with open(infile, "r") as tsv_in_file:
             reader = csv.DictReader(tsv_in_file, delimiter='\t', fieldnames=FIELDNAMES)
             for item in reader:
-                print(item['num_boxes'])
                 item['num_boxes'] = int(item['num_boxes'])
                 item['boxes'] = bytes(item['boxes'], 'utf')
                 item['features'] = bytes(item['features'], 'utf')
