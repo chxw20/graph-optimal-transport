88471992f2ed242f2b7b87a2a08e25ebc67cb913
chxw20 <xiwenche@andrew.cmu.edu>
2021-03-02 17:39:45 +0800

    json

--- BAN_vqa/dataset.py
+++ BAN_vqa/dataset.py
@@ -678,7 +678,8 @@ def _load_kairos(dataset, img_id2idx, bbox, pos_boxes, topic_doc_json, topic=Non
                     try:
                         entity_idx = utils.find_sublist(sentence.split(' '), phrase.split(' '))
                     except:
-                        entity_idx = utils.find_sublist(sentence.split(' '), (phrase + '.').split(' '))
+                        phrase = phrase + '.'
+                        entity_idx = utils.find_sublist(sentence.split(' '), phrase.split(' '))
                     assert 0 <= entity_idx, f"entity_idx = {entity_idx}, entity = {phrase} \nsentence = {sentence}, info = {info}"
 
                     if not entity_id in target_bboxes:
