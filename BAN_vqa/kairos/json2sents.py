ccd44263d1974a431d4bc61f98553c49f96a7272
chxw20 <xiwenche@andrew.cmu.edu>
2021-03-11 11:36:44 +0800

    json2sents

--- BAN_vqa/kairos/json2sents.py
+++ BAN_vqa/kairos/json2sents.py
@@ -4,7 +4,7 @@ import os
 
 from collections import defaultdict
 
-def process(fpath):
+def process(fpath, name=1):
 
     flickr_ent_type_map = {'people':0,'clothing':1,'bodyparts':2,'animals':3,'vehicles':4,'instruments':5,'scene':6,'other':7}
     with open("data/kairos_flickr_ent_mapping.txt", 'r') as f:
@@ -17,6 +17,9 @@ def process(fpath):
     sents = data["sentences"]
     ents = data["entities"]
 
+    corefs = [rel for rel in data["relations"] if "coreference" in rel["type"]]
+    coref2name = {}
+
     results = []
     for sent in sents:
         ent = [e for e in ents if e["id"].startswith(sent["id"] + '-')]
@@ -26,30 +29,51 @@ def process(fpath):
         new_sent = sent["text"]
         new_ent = []
         for i in range(len(ent)):
+            # find overlap with current entities for the current sentence
             overlap = False
             for j in range(len(new_ent)):
                 if not (ent[i]["offset"] + ent[i]["length"] < new_ent[j]["offset"] or ent[i]["offset"] > new_ent[j]["offset"] + new_ent[j]["length"]):
                     overlap = True
+                    # j stores the overlap entity
                     break
             if overlap:
                 # print(ent[i]["text"], " ======= ", new_ent[j]["text"])
-                if ent[i]["length"] < new_ent[j]["length"]:
-                    new_ent[j] = ent[i]
+                if ent[i]["length"] >= new_ent[j]["length"]:
+                    # new_ent[j] = ent[i]
+                    # discard ent[i]
+                    continue
+            # find coreference
+            if ent[i]["id"] in coref2name:
+                ent[i]["ent_name"] = coref2name[ent[i]["id"]]
+            else:
+                for rel in corefs:
+                    if ent[i]["id"] in rel["args"]:
+                        for arg in rel["args"]:
+                            coref2name[arg] = name
+                        ent[i]["ent_name"] = name
+                        name += 1
+                        break
+            # add entity
+            if ent[i]["id"] not in coref2name:
+                ent[i]["ent_name"] = name
+                name += 1
+
+            if overlap:
+                new_ent[j] = ent[i]
             else:
                 new_ent.append(ent[i])
+
         new_ent = sorted(new_ent, key=lambda e: e["offset"])
-        count = 1
         for e in new_ent:
             if e["text"] == '':
                 continue
             l = e["offset"] - offset
             r = l + e["length"]
-            new_sent = new_sent[:l] + f"[/EN#{count}/{kairos_to_flickr_ent_map[e['type']]} " + new_sent[l:r] + "]" + new_sent[r:]
-            offset -= (8 + len(str(count)) + len(kairos_to_flickr_ent_map[e['type']]))
-            count += 1
+            new_sent = new_sent[:l] + f"[/EN#{e['ent_name']}/{kairos_to_flickr_ent_map[e['type']]} " + new_sent[l:r] + "]" + new_sent[r:]
+            offset -= (8 + len(str(e["ent_name"])) + len(kairos_to_flickr_ent_map[e["type"]]))
         results.append(new_sent)
 
-    return results
+    return results, name
 
 
 
@@ -60,8 +84,19 @@ if __name__ == "__main__":
     fnames = os.listdir(f"data/{dataset}/json_output/primitives")
     if not os.path.exists(f"data/{dataset}/json_output/ent_sents"):
         os.makedirs(f"data/{dataset}/json_output/ent_sents")
-    for fname in fnames:
-        sents = process(f"data/{dataset}/json_output/primitives/{fname}")
-        with open(f"data/{dataset}/json_output/ent_sents/{fname.replace('json', 'txt')}", 'w') as f:
-            for line in sents:
-                f.write(line + '\n')
+
+    with open(f"data/{dataset}/topic_mapping.txt", 'r') as f:
+        topic2docs = defaultdict(list)
+        for line in f:
+            line = line.strip().split('\t')
+            topic2docs[line[1]].append(line[0])
+
+    for topic in topic2docs:
+        print(f"processing topic {topic} ...")
+        name = 1
+        topic_fnames = [f"{doc}.json" for doc in topic2docs[topic] if f"{doc}.json" in fnames]
+        for fname in topic_fnames:
+            sents, name = process(f"data/{dataset}/json_output/primitives/{fname}", name)
+            with open(f"data/{dataset}/json_output/ent_sents/{fname.replace('json', 'txt')}", 'w') as f:
+                for line in sents:
+                    f.write(line + '\n')
